ed9cf7777c33c817d5255970155855ad6b044f18
Abdul Wahid Shaikh <ashaikh@starbucks.com>
2019-10-29 15:53:01 -0700

    entered

--- CoreTest/Startup.cs
+++ CoreTest/Startup.cs
@@ -16,6 +16,7 @@ namespace CoreTest
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
